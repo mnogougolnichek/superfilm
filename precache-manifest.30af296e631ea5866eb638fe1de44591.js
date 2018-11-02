f27c6c713b1512ea9a8e52ff4031e77b87a3ba07
mnogougolnichek <mnogougolnichek@gmail.com>
2018-11-02 16:11:58 +0200

    Updates

---
+++ precache-manifest.30af296e631ea5866eb638fe1de44591.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "03d0915f991aa6925899710fa4df385b",
+    "url": "/superfilm/static/media/infoimg.03d0915f.jpg"
+  },
+  {
+    "revision": "a79636841a195883cc21a01cda4e6c8b",
+    "url": "/superfilm/static/media/header.a7963684.jpg"
+  },
+  {
+    "revision": "60eeefe1fa74ba7b396ce7f5ca986bcd",
+    "url": "/superfilm/static/media/chevronleft.60eeefe1.svg"
+  },
+  {
+    "revision": "12ff11717ad74827001d",
+    "url": "/superfilm/static/js/runtime~main.12ff1171.js"
+  },
+  {
+    "revision": "70bab760aa1ba5ce9d8f",
+    "url": "/superfilm/static/js/main.70bab760.chunk.js"
+  },
+  {
+    "revision": "6ef38f9c6784cb6d5777",
+    "url": "/superfilm/static/js/1.6ef38f9c.chunk.js"
+  },
+  {
+    "revision": "70bab760aa1ba5ce9d8f",
+    "url": "/superfilm/static/css/main.e871059c.chunk.css"
+  },
+  {
+    "revision": "6ef38f9c6784cb6d5777",
+    "url": "/superfilm/static/css/1.4400d1b3.chunk.css"
+  },
+  {
+    "revision": "4d9e3692156424b1e65b13dfa5ad308f",
+    "url": "/superfilm/index.html"
+  }
+];
